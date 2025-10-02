035e08b7ce26957dc02059774f7eea833ee0e08c
Stampede <thestampede2011@gmail.com>
2025-10-02 13:50:52 -0500

    sidebar and index updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			title: 'SkiesDev Docs',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/pokeskies' },
+				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/cgBww275Fg' },
+				{ icon: 'heart', label: 'Ko-Fi', href: 'https://ko-fi.com/stampede2011' }
+			],
+			plugins: [
+				starlightSidebarTopics([
+					{
+						label: 'Guides',
+						link: '/guides/',
+						icon: 'open-book',
+						items: ['guides/example'],
+					},
+					{
+						label: 'Reference',
+						link: '/reference/',
+						icon: 'information',
+						items: ['reference/example'],
+					},
+				]),
 			],
 		}),
 	],
